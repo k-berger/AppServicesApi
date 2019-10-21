6df36389ebe6e8e16567343aab85f2e0e21b7fa0
Kristof Berger <kristof.berger@instana.com>
2019-10-21 18:52:37 +0200

    Projektdateien hinzufügen.

---
+++ CoreAppServiceApp/Controllers/DiscoveryController.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+
+namespace CoreAppServiceApp.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class DiscoveryController : ControllerBase
+    {
+        public Dictionary<string, string> Get()
+        {
+            var result = new Dictionary<string, string>();
+            result.Add("Machine", Environment.MachineName);
+            result.Add("OS", Environment.OSVersion.ToString());
+            return result;
+        }
+    }
+}
